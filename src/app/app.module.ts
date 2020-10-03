ccbb7b4ffd5088877f06bbe563275d9665bcf6b3
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-10-03 11:11:34 +0300

    Added the apiUrl in the environment.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GithubComponent } from './github/github.component';
+import { GithubFormComponent } from './github-form/github-form.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GithubComponent,
+    GithubFormComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
