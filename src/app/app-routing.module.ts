8e855fd0a7e8a6fe980cab44857613650507b32a
Ayebale Nelly <ayebalenelly26@gmail.com>
2020-10-04 14:54:48 +0300

    Adding routes to the navigation bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GithubComponent } from './github/github.component';
+import { RepositoryComponent } from './repository/repository.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'users', component: GithubComponent },
+  { path: 'repositories', component: RepositoryComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
